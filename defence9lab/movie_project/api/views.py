47cf8f11bb3796973f6b0ccf84a1bb113ccbe113
keneskhqn <alkhaydar17@icloud.com>
2026-04-09 10:51:19 +0500

    Delete defence9lab directory

--- defence9lab/movie_project/api/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from .models import Movie
-from .serializer import MovieSerializer
-
-class MovieViewSet(viewsets.ModelViewSet):
-    queryset = Movie.objects.all()
-    serializer_class = MovieSerializer
